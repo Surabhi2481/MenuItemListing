d3483031812043b4c255eca24c9cd3ce7d9f4fb2
Surabhi2481 <surabhibbsr2481@gmail.com>
2020-07-25 12:42:44 +0530

    source code added

---
+++ Controllers/MenuItemController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuItemListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        // GET: api/MenuItem
+        public static List<MenuItem> menuItemList = new List<MenuItem>()
+        {
+            new MenuItem{Id=1, Name = "Sandwich",FreeDelivery = true, Price = 25.00, DateOfLaunch = Convert.ToDateTime("2020-07-24"),Active = true},
+            new MenuItem{Id=2, Name = "Noodeles",FreeDelivery = true, Price = 50.00, DateOfLaunch = Convert.ToDateTime("2020-07-24"),Active = true},
+            new MenuItem{Id=3, Name = "Pizza",FreeDelivery = true, Price = 120.00, DateOfLaunch = Convert.ToDateTime("2020-07-24"),Active = true},
+            new MenuItem{Id=4, Name = "Burger",FreeDelivery = true, Price = 60.00, DateOfLaunch = Convert.ToDateTime("2020-07-24"),Active = true},
+            new MenuItem{Id=5, Name = "Hot Dog",FreeDelivery = true, Price = 80.00, DateOfLaunch = Convert.ToDateTime("2020-07-24"),Active = true}
+
+        };
+
+        [HttpGet]
+        public IEnumerable<MenuItem> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            return menuItemList.ToList();
+        }
+
+        // GET: api/MenuItem/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            //return "value";
+            MenuItem item = menuItemList.Where(x => x.Id == id).FirstOrDefault();
+            return Convert.ToString(item.Name);
+        }
+    }
+}
+
---
+++ MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuItemListing
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Free Delivery")]
+        public Boolean FreeDelivery { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfLaunch { get; set; }
+
+        public Boolean Active { get; set; }
+
+    }
+}
